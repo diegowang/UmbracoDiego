e24a86eab2120152c56b82e7643bc4f1b37447d3
Diego Wang <diegowang@yahoo.com>
2015-03-27 15:41:29 -0400

    Initial commit

---
+++ src/UmbracoDiego/Models/Lunch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UmbracoDiego.Models
+{
+    public class Lunch
+    {
+        public int UmbracoID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string LunchChoice { get; set; }
+    }
+}
